be2f056840ecd83466c58e82f4a191c80872cce3
jnims <jmnims@gmail.com>
2015-09-05 20:48:42 -0400

    Removed some redundant code from dec2bin Storing pow() result in a variable did not result in increased performance

--- BaseConverter/Entrypoint.cpp
+++ BaseConverter/Entrypoint.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
-// Supports up to 32-bit integers
-string dec2bin(unsigned int inputNumber, const int exponent = sizeof(int) * 8 - 1)
-{
-    if (exponent < 0)               // base case
-        return "";
-    int binNum = pow(2, exponent);  // perform the operation only once to save time
-    if (inputNumber < binNum)
-        return "0" + dec2bin(inputNumber, exponent - 1);
-    else
-        return "1" + dec2bin(inputNumber - binNum, exponent - 1);
-}
-
+string dec2bin(const unsigned long inputNumber, const int power = sizeof(long) * 8 - 1);
 
 void main()
 {
 
+}
+
+// Supports at least 32-bit unsigned integers (system dependent)
+string dec2bin(const unsigned long inputNumber, const int power)
+{
+    if (power < 0) return "";                        // base case
+    return inputNumber < pow(2, power) ?
+        "0" + dec2bin(inputNumber, power - 1) :
+        "1" + dec2bin(inputNumber - pow(2, power), power - 1);
 }
